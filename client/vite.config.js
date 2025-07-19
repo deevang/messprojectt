d47c268a2fbc8c2b0cb4c08a74c5e0cd3f1171f4
aryantanwarr <91049545+aryantanwarr@users.noreply.github.com>
2025-07-19 18:22:22 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // --- INSERT THE NEW LINES HERE ---
+  build: {
+    outDir: 'dist',
+  },
+  // ---------------------------------
+
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
